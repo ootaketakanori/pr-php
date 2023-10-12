e3fb789c77d8b6714d1cd92478f31d0db38f8fd4
takanori ootake <mine19876293@gmail.com>
2023-10-12 16:06:27 +0900

    new

---
+++ src/php10/result.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/santize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/result.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/php10/index.php">
+                World Clock
+            </a>
+        </div>
+    </header>
+    <main>
+
+    </main>
+</body>
+
+</html>
